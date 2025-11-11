5de718e1696176fe210640b3a19a4d6570350b57
Raj Patel <126887227+rajpat739407@users.noreply.github.com>
2025-11-11 14:00:19 +0530

    Modify main.py to include system path
    Add system path to allow module imports.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from app.routes import movie_routes
